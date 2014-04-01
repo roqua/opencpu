0b2911bd85755db389911338fd61a526ab57b059
Ivan Malykh <ivan@roqua.nl>
2014-04-01 14:30:41 +0200

    added implementation

--- lib/roqua/opencpu.rb
+++ lib/roqua/opencpu.rb
@@ -1,7 +1,25 @@
+require 'httparty'
+require 'yajl'
 require "roqua/opencpu/version"
 
+begin
+  require "pry"
+rescue LoadError
+end
+
 module Roqua
-  module Opencpu
-    # Your code goes here...
+  module OpenCPU
+    class UnsupportedHTTPMethod < StandardError; end
+
+    include HTTParty
+
+    format   :json
+    headers  'Content-Type' => 'application/json'
+    base_uri 'http://10.210.50.11/ocpu'
+
+    def self.execute(package, function, data, method = :post)
+      raise UnsupportedHTTPMethod if method != :post
+      self.send(method, "/library/#{package}/R/#{function}/json", body: data)
+    end
   end
 end
