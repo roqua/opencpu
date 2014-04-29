642f7754436ab2ae08336b282377be193ebd00c4
Ivan Malykh <ivan@roqua.nl>
2014-04-29 18:02:37 +0200

    refactoredclient  to support the two-step request to get the resources

--- lib/opencpu/client.rb
+++ lib/opencpu/client.rb
@@ -2,8 +2,8 @@ module OpenCPU
   class Client
     class UnsupportedHTTPMethod < StandardError; end
     include HTTParty
-    format   :json
-    headers  'Content-Type' => 'application/json'
+
+    attr_accessor :location
 
     def initialize
       self.class.base_uri OpenCPU.configuration.endpoint_url
@@ -18,10 +18,52 @@ module OpenCPU
       }
     end
 
-    def execute(package, function, data = {}, method = :post)
-      raise UnsupportedHTTPMethod if unsupported_http_method? method
-      options = default_options.merge body: data.to_json
-      self.class.send(method, "/library/#{package}/R/#{function}/json", options)
+    def execute(package, function, data = {})
+      options = default_options.merge body: data.to_json, headers: { "Content-Type" => 'application/json' }
+      response = self.class.send(:post, "/library/#{package}/R/#{function}/json", options)
+      if response.ok?
+        return JSON.parse(response.body)
+      else
+        raise 'Error parsing JSON from OpenCPU'
+      end
+    end
+
+    def prepare(package, function, data = {})
+      options = default_options.merge body: data.to_json, headers: { "Content-Type" => 'application/json' }
+      response = self.class.post("/library/#{package}/R/#{function}", options)
+      @location ||= response.headers['location']
+    end
+
+    def graphics(type = :svg)
+      self.class.get "#{@location}graphics/1/#{type.to_s}"
+    end
+
+    def value
+      self.class.get "#{@location}R/.val"
+    end
+
+    def stdout
+      self.class.get "#{@location}stdout"
+    end
+
+    def warnings
+      self.class.get "#{@location}warnings"
+    end
+
+    def source
+      self.class.get "#{@location}source"
+    end
+
+    def console
+      self.class.get "#{@location}console"
+    end
+
+    def info
+      self.class.get "#{@location}info"
+    end
+
+    def reset
+      @location = nil
     end
 
     private
--- spec/lib/opencpu/client_spec.rb
+++ spec/lib/opencpu/client_spec.rb
@@ -42,9 +42,92 @@ describe OpenCPU::Client do
 
     it 'returns a correct HMAC response' do
       VCR.use_cassette :digest_hmac do
-        response = client.execute('digest', 'hmac', key: 'baz', object: 'qux')
+        response = client.execute('digest', 'hmac', { key: 'baz', object: 'qux' })
         expect(response[0]).to eq '22e2a7a268bf076801eefe7cd0119bb9'
       end
     end
   end
+
+  it 'defines #prepare and resource getters' do
+    expect(described_class.new).to respond_to :prepare
+    expect(described_class.new).to respond_to :value
+    expect(described_class.new).to respond_to :stdout
+    expect(described_class.new).to respond_to :warnings
+    expect(described_class.new).to respond_to :info
+    expect(described_class.new).to respond_to :console
+  end
+
+  describe 'preparing and getting resources' do
+    before do
+      OpenCPU.configure do |config|
+        config.endpoint_url = 'https://public.opencpu.org/ocpu'
+        config.username     = 'foo'
+        config.password     = 'bar'
+      end
+    end
+    let(:client) { described_class.new }
+
+    it 'sends initial request to OpenCPU and gets cache location' do
+      VCR.use_cassette :prepare do
+        client.prepare('digest', 'hmac', { key: "baz", object: "qux" })
+        expect(client.location).to eq "https://public.opencpu.org/ocpu/tmp/x0997a7e8eb/"
+      end
+    end
+
+    it 'returns cached R value' do
+      VCR.use_cassette :get_value do
+        client.reset
+        client.prepare('digest', 'hmac', { key: "baz", object: "qux" })
+        response = client.value
+        expect(response.body).to include("22e2a7a268bf076801eefe7cd0119bb9")
+      end
+    end
+
+    it 'returns cached stdout' do
+      VCR.use_cassette :get_stdout do
+        client.reset
+        client.prepare('digest', 'hmac', { key: "baz", object: "qux" })
+        response = client.stdout
+        expect(response.body).to include("22e2a7a268bf076801eefe7cd0119bb9")
+      end
+    end
+
+    it 'returns cached warnings' do
+      VCR.use_cassette :get_warnings do
+        client.reset
+        client.prepare('digest', 'hmac', { key: "baz", object: "qux" })
+        response = client.warnings
+        expect(response.body).to include("the condition has length > 1")
+      end
+    end
+
+    it 'returns cached info' do
+      VCR.use_cassette :get_info do
+        client.reset
+        client.prepare('digest', 'hmac', { key: "baz", object: "qux" })
+        response = client.info
+        expect(response.body).to include("R version")
+      end
+    end
+
+    it 'returns cached console input' do
+      VCR.use_cassette :get_console do
+        client.reset
+        client.prepare('digest', 'hmac', { key: "baz", object: "qux" })
+        response = client.console
+        expect(response.body).to include("hmac(key = \"baz\", object = \"qux\"")
+      end
+    end
+
+    it 'returns cached graphics' do
+      VCR.use_cassette :get_graphics do
+        client.reset
+        client.prepare('graphics', 'plot', { x: [5, 6, 7, 8, 9, 10], y: [10, 8, 11, 1, 4, 3] })
+
+        response = client.graphics
+        expect(response.body).to include("svg xmlns")
+      end
+    end
+  end
+
 end
