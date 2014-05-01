cfb777872c597076f8bb748f56b9829d51e3ca2a
Ivan Malykh <ivan@roqua.nl>
2014-05-01 16:39:23 +0200

    fixed specs to match fixtures and refactorings

--- spec/lib/opencpu/client_spec.rb
+++ spec/lib/opencpu/client_spec.rb
@@ -12,6 +12,10 @@ describe OpenCPU::Client do
     expect(described_class.ancestors).to include HTTParty
   end
 
+  it 'defines #execute' do
+    expect(described_class.new).to respond_to :execute
+  end
+
   it 'defines #prepare' do
     expect(described_class.new).to respond_to :prepare
   end
@@ -40,26 +44,33 @@ describe OpenCPU::Client do
 
     it 'returns a DelayedCalculation with correct path' do
       VCR.use_cassette :prepare do
-        expect(delayed_calculation.location).to eq "https://public.opencpu.org/ocpu/tmp/x0997a7e8eb/"
+        expect(delayed_calculation.location).to eq "https://public.opencpu.org/ocpu/tmp/x0a79915480/"
       end
     end
   end
 
-  # describe '#execute' do
-  #   before do
-  #     OpenCPU.configure do |config|
-  #       config.endpoint_url = 'https://public.opencpu.org/ocpu'
-  #     end
-  #   end
+  describe '#execute' do
+    let(:client) { described_class.new }
 
-  #   let(:client) { described_class.new }
+    it 'is used to quickly return JSON results' do
+      VCR.use_cassette :animation_flip_coin, record: :new_episodes do
+        response = client.execute(:animation, 'flip.coin')
+        expect(response).to eq "freq" => [0.56, 0.44], "nmax" => [50]
+      end
+    end
 
-  #   it 'returns a correct HMAC response' do
-  #     VCR.use_cassette :digest_hmac do
-  #       response = client.execute('digest', 'hmac', { key: 'baz', object: 'qux' })
-  #       expect(response[0]).to eq '22e2a7a268bf076801eefe7cd0119bb9'
-  #     end
-  #   end
-  # end
+    it 'accepts R-function parameters as data' do
+      VCR.use_cassette :digest_hmac, record: :new_episodes do
+        response = client.execute(:digest, :hmac, key: 'baz', object: 'qux')
+        expect(response).to eq ["22e2a7a268bf076801eefe7cd0119bb9"]
+      end
+    end
+
+    it 'raises 400 with body' do
+      VCR.use_cassette :digest_hmac_no_parameters do
+        expect { client.execute(:digest, :hmac) }.to raise_error
+      end
+    end
+  end
 
 end
