03f96b25f7f8c00938df738ffc9aac9f09e4578f
Ivan Malykh <ivan@roqua.nl>
2014-05-01 13:55:41 +0200

    refactored client

--- lib/opencpu/client.rb
+++ lib/opencpu/client.rb
@@ -1,13 +1,30 @@
 module OpenCPU
   class Client
-    class UnsupportedHTTPMethod < StandardError; end
     include HTTParty
-
-    attr_accessor :location
 
     def initialize
       self.class.base_uri OpenCPU.configuration.endpoint_url
     end
+
+    # def execute(package, function, data = {})
+    #   options = default_options.merge body: data.to_json, headers: { "Content-Type" => 'application/json' }
+    #   response = self.class.send(:post, "/library/#{package}/R/#{function}/json", options)
+    #   if response.ok?
+    #     return JSON.parse(response.body)
+    #   else
+    #     raise 'Error parsing JSON from OpenCPU'
+    #   end
+    # end
+
+    def prepare(package, function, data = {})
+      options   = default_options.merge body: data.to_json, headers: { "Content-Type" => 'application/json' }
+      response  = self.class.post("/library/#{package}/R/#{function}", options)
+      location  = response.headers['location']
+      resources = response.body.split(/\n/)
+      OpenCPU::DelayedCalculation.new(location, resources)
+    end
+
+    private
 
     def default_options
       {
