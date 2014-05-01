3c5d0dff4d733201cdd49da932f917f09189314c
Ivan Malykh <ivan@roqua.nl>
2014-05-01 16:38:47 +0200

    refactored http interaction

--- lib/opencpu/client.rb
+++ lib/opencpu/client.rb
@@ -6,33 +6,38 @@ module OpenCPU
       self.class.base_uri OpenCPU.configuration.endpoint_url
     end
 
-    # def execute(package, function, data = {})
-    #   options = default_options.merge body: data.to_json, headers: { "Content-Type" => 'application/json' }
-    #   response = self.class.send(:post, "/library/#{package}/R/#{function}/json", options)
-    #   if response.ok?
-    #     return JSON.parse(response.body)
-    #   else
-    #     raise 'Error parsing JSON from OpenCPU'
-    #   end
-    # end
+    def execute(package, function, data = {})
+      process_query package_url(package, function, :json), data do |response|
+        JSON.parse(response.body)
+      end
+    end
 
     def prepare(package, function, data = {})
-      options   = default_options.merge body: data.to_json, headers: { "Content-Type" => 'application/json' }
-      response  = self.class.post("/library/#{package}/R/#{function}", options)
-      location  = response.headers['location']
-      resources = response.body.split(/\n/)
-      OpenCPU::DelayedCalculation.new(location, resources)
+      process_query package_url(package, function), data do |response|
+        location  = response.headers['location']
+        resources = response.body.split(/\n/)
+        OpenCPU::DelayedCalculation.new(location, resources)
+      end
     end
 
     private
 
-    def default_options
-      {
-        basic_auth: {
-          username: OpenCPU.configuration.username,
-          password: OpenCPU.configuration.password
-        }
-      }
+    def process_query(url, data, &block)
+      options   = { body: data.to_json, headers: { "Content-Type" => 'application/json' } }
+      response  = self.class.post(url, options)
+      
+      case response.code
+      when 200..201
+        return yield(response)
+      when 400
+        raise '400: Bad Request\n' + response.body
+      else
+        raise 'Error'
+      end
+    end
+
+    def package_url(package, function, format = nil)
+      "/library/#{package.to_s}/R/#{function.to_s}/#{format.to_s}"
     end
   end
 end
